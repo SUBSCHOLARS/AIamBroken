90bbd248c1e178ea9c71b44db456da27653cd815
Kota Hatanaka <kenkotan7722@gmail.com>
2025-06-08 18:56:32 +0900

    ドラッグとクリックしたら前に表示される処理

--- AIamBroken/Assets/Scripts/WindowDragHandler.cs
+++ AIamBroken/Assets/Scripts/WindowDragHandler.cs
@@ -1,9 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class WindowDragHandler : MonoBehaviour
+public class WindowDragHandler : MonoBehaviour, IDragHandler, IPointerDownHandler
 {
+    [SerializeField] private RectTransform windowRectTransform;
+    private Canvas canvas;
+
+    private void Awake()
+    {
+        //親Canvasの参照を取得
+        canvas = GetComponentInParent<Canvas>();   
+    }
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //クリックされたウィンドウを一番手前に表示
+        windowRectTransform.SetAsLastSibling();
+    }
+    public void OnDrag(PointerEventData eventData)
+    {
+        //マウスの移動量に応じてウィンドウを動かす
+        windowRectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
     // Start is called before the first frame update
     void Start()
     {
